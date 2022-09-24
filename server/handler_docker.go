14562910771920b5044ca66b0d5883d8098a3a5b
zchary <zcharyma@gmail.com>
2022-09-24 16:18:12 +0800

    update ssm pkg

--- server/handler_docker.go
+++ server/handler_docker.go
@@ -1,43 +1,20 @@
 package server
 
 import (
-	"errors"
 	"net/http"
-
-	"github.com/docker/docker/api/types"
 )
 
-func ContainerList(_ http.ResponseWriter, r *http.Request) (any, error) {
+type Transport struct {
+	HTTPTransport http.Transport
+}
 
-	arr := make([]*types.Container, 0)
-	arr = append(arr, &types.Container{
-		Names: []string{"test"},
-	})
+func NewTransport() (*Transport, error) {
+	return &Transport{
+		HTTPTransport: http.Transport{},
+	}, nil
+}
+
+func (t *Transport) ContainerProxy(w http.ResponseWriter, r *http.Request) (any, error) {
 
 	return nil, nil
-}
-
-func ContainerCreate(_ http.ResponseWriter, r *http.Request) (any, error) {
-	return nil, errors.New("not implemented")
-}
-func ContainerRemove(_ http.ResponseWriter, r *http.Request) (any, error) {
-	return nil, errors.New("not implemented")
-}
-func ContainerRename(_ http.ResponseWriter, r *http.Request) (any, error) {
-	return nil, errors.New("not implemented")
-}
-func ContainerStart(_ http.ResponseWriter, r *http.Request) (any, error) {
-	return nil, errors.New("not implemented")
-}
-func ContainerStop(_ http.ResponseWriter, r *http.Request) (any, error) {
-	return nil, errors.New("not implemented")
-}
-func ContainerRestart(_ http.ResponseWriter, r *http.Request) (any, error) {
-	return nil, errors.New("not implemented")
-}
-func ContainerKill(_ http.ResponseWriter, r *http.Request) (any, error) {
-	return nil, errors.New("not implemented")
-}
-func ContainersPrune(_ http.ResponseWriter, r *http.Request) (any, error) {
-	return nil, errors.New("not implemented")
 }
